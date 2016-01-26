1610b3ebfa963308f8ecf66d33f22cf1e2cd0205
KKudryavtsev <konstantin.k.ed@gmail.com>
2016-01-26 15:15:15 +0000

    added sensitive data (encrypted with ansible-vault)

--- scripts/vanity_service.sh
+++ scripts/vanity_service.sh
@@ -1,1 +1,1 @@
-ansible-playbook ethcore_io.yml -vvvv -i group_vars/inventory/main --tags vanity-service
+ansible-playbook ethcore_io.yml -vvvv -i group_vars/inventory/main --tags vanity-service --vault-password-file ~/.vault_pass.txt
