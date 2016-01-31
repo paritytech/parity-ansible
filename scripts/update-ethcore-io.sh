c9f64e34a7196f0c36ef7be74d078a52360b73a4
KKudryavtsev <konstantin.k.ed@gmail.com>
2016-01-31 17:47:43 +0000

    updates

---
+++ scripts/update-ethcore-io.sh
@@ -0,0 +1,1 @@
+ansible-playbook ethcore_io.yml -vvvv -i group_vars/inventory/main --tags update --vault-password-file ~/.vault_pass.txt
--- scripts/vanity_service.sh
+++ scripts/vanity_service.sh
@@ -1,1 +1,1 @@
-ansible-playbook ethcore_io.yml -vvvv -i group_vars/inventory/main --tags vanity-service --vault-password-file ~/.vault_pass.txt
+ansible-playbook vanity_service.yml -vvvv -i group_vars/inventory/main --vault-password-file ~/.vault_pass.txt
