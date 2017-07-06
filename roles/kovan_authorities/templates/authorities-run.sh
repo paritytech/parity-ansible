c8f04c3ac68122242885c3446e9c9dd4ff83b9b9
Konstantin <konstantin.k.ed@gmail.com>
2017-07-06 15:40:02 +0100

    small update

--- roles/kovan_authorities/templates/authorities-run.sh
+++ roles/kovan_authorities/templates/authorities-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-parity --chain kovan --no-dapps --no-ui --password pass --engine-signer {{signer_address}} --reseal-on-txs none --force-sealing --jsonrpc-apis web3,eth,parity,parity_set,net,traces,rpc  --gas-floor-target 5000000 --auto-update=all --no-warp --allow-ips public --no-periodic-snapshot --no-discovery
+parity --chain kovan --no-dapps --no-ui --password pass --engine-signer {{signer_address}} --reseal-on-txs none --force-sealing --jsonrpc-apis web3,eth,parity,parity_set,net,traces,rpc  --gas-floor-target 7000000 --auto-update=all --no-warp --allow-ips public --no-periodic-snapshot --no-discovery
