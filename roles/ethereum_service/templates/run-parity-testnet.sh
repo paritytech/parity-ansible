70de9e9f9001c73725da975970f55aa2342d93d6
David Dorgan <david@parity.io>
2017-08-02 22:28:07 +0100

    Add no --no-ws option because you cannot use port 8546 otherwise.

--- roles/ethereum_service/templates/run-parity-testnet.sh
+++ roles/ethereum_service/templates/run-parity-testnet.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-parity --warp --chain ropsten --jsonrpc-port 8546 --no-dapps --port 30304 --no-ui
+parity --warp --chain ropsten --jsonrpc-port 8546 --no-dapps --port 30304 --no-ws --no-ui
